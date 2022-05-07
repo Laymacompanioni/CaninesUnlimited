69393a9d1775ac98d80e7cf195b41ee3a864b439
Layma Companioni <layma.companioni@dcmail.ca>
2022-05-07 17:17:56 -0400

    added scrolling for header

--- js/script.js
+++ js/script.js
@@ -2,4 +2,15 @@ $(document).ready(function(){
     $('#navbar-toggle').click(function(){
         $('.navbar-collapse').slideToggle(400);
     });
+
+    // navbar - change bg on scroll
+    $(window).scroll(function(){
+        let pos =$(window).scrollTop();
+        if (pos > 100){
+            $('.navbar').addClass('cng-navbar');
+        }else{
+            $('.navbar').removeClass('cng-navbar');
+        }
+    });
+
 });
