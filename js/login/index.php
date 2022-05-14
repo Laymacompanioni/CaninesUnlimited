a27feedd4e3bdb4fbd74319d1dca3adf2a014840
Layma Companioni <layma.companioni@dcmail.ca>
2022-05-13 23:52:37 -0400

    php

---
+++ js/login/index.php
@@ -0,0 +1,18 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website</title>
+</head>
+<body>
+    <a href="logout.php">logoout</a>
+    <h1>This is the index page</h1>
+    <br>
+    Hello, Username!
+</body>
+</html>
